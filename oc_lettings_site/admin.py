5a8d122fcc81bb4b371050bfcf0d5e54030ff8b0
John <john.t.singer@gmail.com>
2023-12-30 15:23:08 +0100

    move admin to each apps lettings and profiles

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from .models import Letting
-from .models import Address
-from .models import Profile
-
-
-admin.site.register(Letting)
-admin.site.register(Address)
-admin.site.register(Profile)
+# Register your models here.

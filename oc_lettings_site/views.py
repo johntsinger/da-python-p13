6f82f59f74787a4cf33110164f958bb1634fe051
John <john.t.singer@gmail.com>
2024-01-03 15:18:43 +0100

    add custom 404 and 500 views, add docstrings and pep8 correction

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+# Quisque molestie quam lobortis leo consectetur ullamcorper non id est.
+# Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
+# vitae dapibus nisi lorem dapibus sem.
+# Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis.
+# Phasellus eleifend ex auctor venenatis tempus.
+# Aliquam vitae erat ac orci placerat luctus.
+# Nullam elementum urna nisi, pellentesque iaculis enim cursus in.
+# Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
+    """Index view."""
     return render(request, 'index.html')
+
+
+def custom_404(request, exception=None, template_name='404.html'):
+    """Error 404 view."""
+    return render(request, template_name, status=404)
+
+
+def custom_500(request, exception=None, template_name='500.html'):
+    """Error 500 view."""
+    return render(request, template_name, status=500)

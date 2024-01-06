b0f17a34f3868529b5da048ff24d52748a24319e
John <john.t.singer@gmail.com>
2024-01-06 10:19:48 +0100

    add sentry debug

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from oc_lettings_site import views
 
 
+def trigger_error(request):
+    """Make sure that Sentry is working"""
+    division_by_zero = 1 / 0
+
+
 handler404 = 'oc_lettings_site.views.custom_404'
 handler500 = 'oc_lettings_site.views.custom_500'
 
@@ -11,4 +16,5 @@ urlpatterns = [
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]

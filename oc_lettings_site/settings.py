dffbc578e47fcdf758400c492eeff0dd61e332cf
John <john.t.singer@gmail.com>
2024-01-23 12:31:39 +0100

    Revert "change global allowed hosts to dev and prod allowed hosts"
    This reverts commit fabf115b59a15696e6fde787fed19064248581d4.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -20,11 +20,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME]
-else:
-    ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
